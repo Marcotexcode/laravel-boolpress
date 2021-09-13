21c1b97e70a6f077e8df92be2b5aa36cef19d358
Marco Tessitore <marcotexcode@gmail.com>
2021-09-13 15:19:21 +0200

    autenticazione

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('guest.welcome');
+    
+});
+
+Auth::routes();
+
+
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function() {
+
+    Route::get('/', 'HomeController@index')->name('index');
+
 });
